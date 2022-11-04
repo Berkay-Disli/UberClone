f78a2a0f0d8e26ae94d80bb69904fcb4e5aedd92
Berkay-Disli <berkaydisli.work@gmail.com>
2022-11-04 15:07:04 +0300

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Berkay Disli on 4.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
